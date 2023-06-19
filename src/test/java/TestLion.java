856b4993d07c11da076043ae0a7e39f0030538e4
Sergei Boyarinov <s.boyarinov@telecontact.ru>
2023-06-19 20:58:13 +0300

    Spring_6 - исправил последние 4 замечания

--- src/test/java/TestLion.java
+++ src/test/java/TestLion.java
@@ -1,78 +1,23 @@
 import com.example.Feline;
 import com.example.Lion;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mockito;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-@RunWith(Parameterized.class)
 public class TestLion {
 
     private Feline feline = Mockito.mock(Feline.class);
     private Lion lion;
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {"Самец", true},
-                {"Самка", false}
-        });
-    }
-
-    private final String typeOfCreature;
-    private final Object expectedData;
-
-
-    public TestLion(String typeOfCreature, Object expectedData) {
-        this.typeOfCreature = typeOfCreature;
-        this.expectedData = expectedData;
-    }
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
-    public void testLionConstructorWithNoException() throws Exception {
-        lion = new Lion(typeOfCreature, new Feline());
+    public void testLionConstructorWithException() throws Exception {
+        exceptionRule.expect(Exception.class);
+        exceptionRule.expectMessage("Используйте допустимые значения пола животного - самей или самка");
 
-        Assert.assertEquals(expectedData, lion.doesHaveMane());
-    }
-
-    @Test
-    public void testLionConstructorWithException() {
-        try {
-            lion = new Lion("Unknown creature", new Feline());
-        } catch (Exception exception) {
-            Assert.assertTrue(true);
-        }
-    }
-
-    @Test
-    public void testGetKittensMethod() throws Exception {
-        lion = new Lion("Самка", feline);
-        when(feline.getKittens()).thenReturn(1);
-        lion.getKittens();
-        verify(feline, Mockito.times(1)).getKittens();
-    }
-
-
-    @Test
-    public void testGetFoodMethod() throws Exception {
-        lion = new Lion("Самка", feline);
-        when(feline.getFood("Хищник")).thenReturn(List.of("value"));
-        lion.getFood();
-        verify(feline, Mockito.times(1)).getFood("Хищник");
-    }
-
-    @Test
-    public void testDoesHaveManeMethod() throws Exception {
-        lion = new Lion("Самка", feline);
-
-        Assert.assertFalse(lion.doesHaveMane());
+        lion = new Lion("Unknown creature", new Feline());
     }
 }
